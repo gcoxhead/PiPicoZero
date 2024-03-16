45ef064aa0bb3a5e8bc5779cf24be8438a2043a9
Gary Coxhead <79542013+gcoxhead@users.noreply.github.com>
2024-03-16 15:30:57 +0000

    Update web_server.py
    update

--- web_server.py
+++ web_server.py
@@ -1,7 +1,22 @@
 import network
 import socket
 from time import sleep
-from picozero import pice_temp_sensor, pico_led
+from picozero import pico_temp_sensor, pico_led
 import machine
 
+ssid = 'VM7274398'
+password = 'tn5Qpstxz7fx'
 
+def connect():
+    #Connect to WLAN
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    while wlan.isconnected() == False:
+        print('Waiting for connection...')
+        sleep(1)
+        
+try:
+    connect()
+except KeyboardInterrupt:
+    machine.reset()
